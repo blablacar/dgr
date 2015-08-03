a1e80e8854d6b6d3b34edece349300482379cbe1
Arnaud Lemaire <alemaire@norad.fr>
2015-08-03 17:25:45 +0200

    fix build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ dir=$( dirname $0 )
 
 rm -Rf ${dir}/target/
 
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o target/darwin-amd64/cnt&
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o target/windows-amd64/cnt.exe
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o target/linux-amd64/cnt
+GOOS=darwin GOARCH=amd64 go build -o target/darwin-amd64/cnt&
+GOOS=windows GOARCH=amd64 go build -o target/windows-amd64/cnt.exe
+GOOS=linux GOARCH=amd64 go build -o target/linux-amd64/cnt
