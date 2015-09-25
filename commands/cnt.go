387322c766f19663d779206b650171be2f221db5
Arnaud Lemaire <alemaire@norad.fr>
2015-09-25 11:47:38 +0200

    update go dep

--- commands/cnt.go
+++ commands/cnt.go
@@ -1,17 +1,25 @@
 package commands
 
 import (
+	"bufio"
 	"github.com/blablacar/cnt/builder"
 	"github.com/blablacar/cnt/config"
 	"github.com/blablacar/cnt/log"
 	"github.com/blablacar/cnt/logger"
+	"github.com/blablacar/cnt/utils"
+	"github.com/coreos/go-semver/semver"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 var buildArgs = builder.BuildArgs{}
 
+const RKT_SUPPORTED_VERSION = "0.8.1"
+
 func Execute() {
 	log.Set(logger.NewLogger())
+	checkRktVersion()
+
 	var rootCmd = &cobra.Command{Use: "cnt"}
 	rootCmd.PersistentFlags().BoolVarP(&buildArgs.Clean, "clean", "c", false, "Clean before doing anything")
 	rootCmd.PersistentFlags().StringVarP(&buildArgs.TargetPath, "target-path", "t", "", "Set target path")
@@ -22,6 +30,32 @@ func Execute() {
 	rootCmd.Execute()
 
 	log.Get().Info("Victory !")
+}
+
+func checkRktVersion() {
+	output, err := utils.ExecCmdGetOutput("rkt")
+	if err != nil {
+		log.Get().Panic("rkt is required in PATH")
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "VERSION:" {
+			scanner.Scan()
+			versionString := strings.TrimSpace(scanner.Text())
+			version, err := semver.NewVersion(versionString)
+			if err != nil {
+				log.Get().Panic("Cannot parse version of rkt", versionString)
+			}
+			supported, _ := semver.NewVersion(RKT_SUPPORTED_VERSION)
+			if version.LessThan(*supported) {
+				log.Get().Panic("rkt version in your path is too old. Require >= " + RKT_SUPPORTED_VERSION)
+			}
+			break
+		}
+	}
+
 }
 
 func discoverAndRunBuildType(path string, args builder.BuildArgs) {
