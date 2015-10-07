2912d14bee34e76d7152c6d1f70ea4f0fcc63ab6
Arnaud Lemaire <alemaire@norad.fr>
2015-10-07 10:42:30 +0200

    do not use overlay for tests

--- builder/aci-test.go
+++ builder/aci-test.go
@@ -69,6 +69,7 @@ func (cnt *Img) Test() {
 		"--insecure-skip-verify=true",
 		"run",
 		"--mds-register=false",
+		"--no-overlay=true",
 		"--volume=result,kind=host,source="+cnt.target+PATH_TESTS+PATH_TARGET+PATH_RESULT,
 		cnt.target+PATH_TESTS+PATH_TARGET+"/image.aci"); err != nil {
 		// rkt+systemd cannot exit with fail status yet
