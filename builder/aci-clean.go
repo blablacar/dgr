c176d28d2407f573cd5097633e0fc53b43abe560
Maxime Fouilleul <maxime.fouilleul@blablacar.com>
2015-10-15 13:32:29 +0000

    Add check dependenies version in aci-clean

--- builder/aci-clean.go
+++ builder/aci-clean.go
@@ -7,7 +7,10 @@ import (
 
 func (cnt *Img) Clean() {
 	log.Get().Info("Cleaning " + cnt.manifest.NameAndVersion)
+	checkVersion := make(chan bool, 1)
+	go cnt.checkLatestVersions(&checkVersion)
 	if err := os.RemoveAll(cnt.target + "/"); err != nil {
 		log.Get().Panic("Cannot clean "+cnt.manifest.NameAndVersion, err)
 	}
+	<-checkVersion
 }
