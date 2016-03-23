4e3054923ad9cc75fb084ff9c0db7f4a92ec3ef8
Arnaud Lemaire <alemaire@norad.fr>
2016-03-23 10:47:23 +0100

    Use random filename for internal import

--- bin-dgr/rkt.go
+++ bin-dgr/rkt.go
@@ -5,6 +5,7 @@ import (
 	"github.com/blablacar/dgr/dist"
 	"github.com/n0rad/go-erlog/logs"
 	"io/ioutil"
+	"math/rand"
 	"os"
 )
 
@@ -34,12 +35,37 @@ func importInternalAci(filename string) {
 	if err != nil {
 		logs.WithE(err).WithField("aci", filepath).Fatal("Cannot found internal aci")
 	}
-	if err := ioutil.WriteFile("/tmp/tmp.aci", content, 0644); err != nil {
+	tmpFile := "/tmp/" + RandStringBytesMaskImpr(20) + ".aci"
+	if err := ioutil.WriteFile(tmpFile, content, 0644); err != nil {
 		logs.WithE(err).WithField("aci", filepath).Fatal("Failed to write tmp aci to /tmp/tmp.aci")
 	}
-	if _, err := Home.Rkt.Fetch("/tmp/tmp.aci"); err != nil { // TODO does not support concurrency
+	defer os.Remove(tmpFile)
+	if _, err := Home.Rkt.Fetch(tmpFile); err != nil { // TODO does not support concurrency
 		logs.WithE(err).Fatal("Failed to import internal image to rkt")
 	}
-	os.Remove("/tmp/tmp.aci")
-	return
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandStringBytesMaskImpr(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
