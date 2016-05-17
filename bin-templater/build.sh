f40f71ce23c3bd41689ec9247da43fa22ea5032b
Arnaud Lemaire <alemaire@norad.fr>
2016-05-17 15:16:05 +0200

    add back upx to templater

--- bin-templater/build.sh
+++ bin-templater/build.sh
@@ -7,4 +7,4 @@ dist=${dir}/../dist
 echo -e "\033[0;32mBuilding bin-templater\033[0m\n"
 
 GOOS=linux GOARCH=amd64 godep go build --ldflags '-extldflags "-static"' -o ${dist}/templater ${dir}
-#upx ${dist}/templater
+upx ${dist}/templater
