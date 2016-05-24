0c308e812e5a6a8f85da1a3fa4f59a49b1c29b74
Arnaud Lemaire <alemaire@norad.fr>
2016-05-24 12:26:45 +0200

    update cdeboostrap version since debian remove the 0.7.3 version

--- examples/debian/aci-debian/runlevels/builder/10.debootstrap.sh
+++ examples/debian/aci-debian/runlevels/builder/10.debootstrap.sh
@@ -4,7 +4,7 @@ set -e
 isLevelEnabled "debug" && set -x
 
 keyringFile=debian-archive-keyring_2014.3_all.deb
-cdebootstrapFile=cdebootstrap-static_0.7.3_amd64.deb
+cdebootstrapFile=cdebootstrap-static_0.7.4_amd64.deb
 gpgvFile=gpgv_1.4.18-7+deb8u1_amd64.deb
 
 mkdir /tmp/debootstrap
