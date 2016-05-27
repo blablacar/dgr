c751eeb36d0dc5fa023eb9c92b3879661bdf8ab3
Arnaud Lemaire <alemaire@norad.fr>
2016-05-27 17:19:01 +0200

    do not push pod manifest if not using maven

--- bin-dgr/pod-push.go
+++ bin-dgr/pod-push.go
@@ -29,19 +29,22 @@ func (p *Pod) Push() error {
 		}
 	}
 
-	if err := common.ExecCmd("curl", "-i",
-		"-F", "r=releases",
-		"-F", "hasPom=false",
-		"-F", "e=pod",
-		"-F", "g=com.blablacar.aci.linux.amd64",
-		"-F", "p=pod",
-		"-F", "v="+p.manifest.Name.Version(),
-		"-F", "a="+p.manifest.Name.ShortName(),
-		"-F", "file=@"+p.target+"/pod-manifest.json",
-		"-u", Home.Config.Push.Username+":"+Home.Config.Push.Password,
-		Home.Config.Push.Url+"/service/local/artifact/maven/content"); err != nil {
+	if Home.Config.Push.Type == "maven" && p.manifest.Name.DomainName() == "aci.blbl.cr" {
+		// TODO this definitely need to be removed
+		if err := common.ExecCmd("curl", "-i",
+			"-F", "r=releases",
+			"-F", "hasPom=false",
+			"-F", "e=pod",
+			"-F", "g=com.blablacar.aci.linux.amd64",
+			"-F", "p=pod",
+			"-F", "v="+p.manifest.Name.Version(),
+			"-F", "a="+p.manifest.Name.ShortName(),
+			"-F", "file=@"+p.target+"/pod-manifest.json",
+			"-u", Home.Config.Push.Username+":"+Home.Config.Push.Password,
+			Home.Config.Push.Url+"/service/local/artifact/maven/content"); err != nil {
 
-		return errs.WithEF(err, p.fields, "Failed to push pod")
+			return errs.WithEF(err, p.fields, "Failed to push pod")
+		}
 	}
 
 	return nil
