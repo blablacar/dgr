8b5bb18be0ad2b4a85ea7d86b8c1312fc884636e
Arnaud Lemaire <alemaire@norad.fr>
2016-06-16 13:42:07 +0200

    fix debian-cassandra install using backports

--- examples/debian/aci-debian-cassandra/runlevels/build/10.install.sh
+++ examples/debian/aci-debian-cassandra/runlevels/build/10.install.sh
@@ -4,9 +4,10 @@ set -e
 isLevelEnabled "debug" && set -x
 
 cat > /etc/apt/sources.list.d/cassandra.list<<EOF
-deb http://ftp.fr.debian.org/debian/ sid main non-free contrib # needed for java8
+deb http://ftp.fr.debian.org/debian/ jessie main non-free contrib # needed for java8
 deb http://www.apache.org/dist/cassandra/debian 30x main
 deb-src http://www.apache.org/dist/cassandra/debian 30x main
+deb http://ftp.debian.org/debian jessie-backports main
 EOF
 
 gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
