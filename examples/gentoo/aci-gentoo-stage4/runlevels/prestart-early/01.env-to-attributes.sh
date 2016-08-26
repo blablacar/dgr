9577460f8465a2b010937504d3d47b0c5c06932a
Arnaud Lemaire <alemaire@norad.fr>
2016-08-26 18:16:32 +0200

    fix gentoo example

--- examples/gentoo/aci-gentoo-stage4/runlevels/prestart-early/01.env-to-attributes.sh
+++ examples/gentoo/aci-gentoo-stage4/runlevels/prestart-early/01.env-to-attributes.sh
@@ -6,5 +6,5 @@ isLevelEnabled "debug" && set -x
 mkdir -p /dgr/attributes/aci-gentoo-stage4
 cat > /dgr/attributes/aci-gentoo-stage4/prestart.yml <<EOF
 default:
-  rootfs: ${ROOTFS}
+  rootfs: "${ROOTFS}"
 EOF
