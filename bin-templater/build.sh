f7aadedc878bae7bfb356c831b4de65c6e6929e8
Arnaud Lemaire <alemaire@norad.fr>
2016-11-28 16:03:34 +0100

    remove godep from bin-templater

--- bin-templater/build.sh
+++ bin-templater/build.sh
@@ -6,5 +6,5 @@ dist=${dir}/../dist
 
 echo -e "\033[0;32mBuilding bin-templater\033[0m\n"
 
-GOOS=linux GOARCH=amd64 godep go build --ldflags '-extldflags "-static"' -o ${dist}/templater ${dir}
+GOOS=linux GOARCH=amd64 go build --ldflags '-extldflags "-static"' -o ${dist}/templater ${dir}
 upx ${dist}/templater
