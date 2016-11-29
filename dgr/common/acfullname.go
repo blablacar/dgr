6477ca5dad5bb9ac9af77845ffee9ddd645ba1b8
Arnaud Lemaire <alemaire@norad.fr>
2016-11-29 14:22:51 +0100

    Revert "support version with ':'"
    This reverts commit f1f11ca5b38b20cbc87aa29899801a7f1f5db227.
    
    dgr can support it, but rkt & push spec does not allow it

--- dgr/common/acfullname.go
+++ dgr/common/acfullname.go
@@ -84,7 +84,7 @@ func (n ACFullname) FullyResolved() (*ACFullname, error) {
 
 /* 1 */
 func (n ACFullname) Version() string {
-	split := strings.SplitN(string(n), ":", 2)
+	split := strings.Split(string(n), ":")
 	if len(split) == 1 {
 		return ""
 	}
