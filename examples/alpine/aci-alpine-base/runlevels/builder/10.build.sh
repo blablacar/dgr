40d9c8c8aa7c0797345c9afbc79450c6b0b330a5
Arnaud Lemaire <alemaire@norad.fr>
2016-12-29 14:28:17 +0100

    bump alpine apk version

--- examples/alpine/aci-alpine-base/runlevels/builder/10.build.sh
+++ examples/alpine/aci-alpine-base/runlevels/builder/10.build.sh
@@ -4,7 +4,7 @@ set -e
 isLevelEnabled "debug" && set -x
 
 mirror=http://nl.alpinelinux.org/alpine/
-version=2.6.7-r0
+version=2.6.8-r1
 
 wget ${mirror}/latest-stable/main/x86_64/apk-tools-static-${version}.apk
 tar -xzf apk-tools-static-*.apk
